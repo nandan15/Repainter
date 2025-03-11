b77577a08be94c1ac8f95006bc0f44c831a3ba50
Nandan Kumar <nandan@likitech.com>
2025-03-11 12:35:57 +0530

    login url fix

--- Repainters/src/app/Shared/Service/Login/login.service.ts
+++ Repainters/src/app/Shared/Service/Login/login.service.ts
@@ -11,13 +11,11 @@ export class LoginService {
     private loginMethod: string;
     
     constructor(private httpClient: HttpClient) { 
-        // Force HTTP protocol with full URL
-        this.loginMethod = "http://localhost:7149/api/Authenticate/login";
+        this.loginMethod = `${environment.backend.baseURL}api/Authenticate/login`;
     }
-    
+
     public login(login: Login): Observable<token> {
         console.log('Login URL being used:', this.loginMethod);
-        // Create a new HttpRequest with the explicit URL
         return this.httpClient.post<token>(this.loginMethod, login, {
             headers: {
                 'Content-Type': 'application/json'
